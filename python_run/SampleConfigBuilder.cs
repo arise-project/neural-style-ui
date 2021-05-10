f7153e52aa2325d92af24979deaecad58a6bc66a
arise-project <pirogov.e@gmail.com>
2021-05-10 16:51:27 +0300

    matrix config

--- python_run/SampleConfigBuilder.cs
+++ python_run/SampleConfigBuilder.cs
@@ -5,7 +5,7 @@ namespace python_run
 {
     public class SampleConfigBuilder
     {
-        public void Save()
+        public void SaveP()
         {
             var config = new PermutationStrategy
             {
@@ -27,7 +27,34 @@ namespace python_run
                 }
             };
            var jsonStr = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-           File.WriteAllText("sample_config.json", jsonStr);
+           File.WriteAllText("p_sample_config.json", jsonStr);
+        }
+        
+
+        public void SaveM()
+        {
+            var config = new MatrixStrategy
+            {
+                Runner = new StartParams {
+                    Runner = "python3",
+                    Script = "neural_style.py"
+                },
+                ScriptArguments = new NSParams{
+                    OutputImage = "test.png",
+                    ImageSize = 800,
+                    Gpu = new int [] { 0 },
+                    Backend = "cudnn",
+                    NumIterations = 800
+                },
+                Storage = new MStorageParams {
+                    ContentSource = "/home/eugene/Pictures/p1/c/",
+                    StyleSource = "/home/eugene/Pictures/p1/s/",
+                    NormalizedSource = "/home/eugene/Pictures/norm/",
+                    Destination = "/home/eugene/Pictures/grid/"
+                }
+            };
+           var jsonStr = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+           File.WriteAllText("m_sample_config.json", jsonStr);
         }
     }
 }
